592de01c15cc476cc31042e01fac18c994df0eb4
Andrew Creahan <CreedyNZ@users.noreply.github.com>
2020-08-03 12:40:29 +1200

    Update to CM9.04

--- teensy/firmware/src/firmware.ino
+++ teensy/firmware/src/firmware.ino
@@ -14,26 +14,15 @@
 #include "geometry_msgs/Twist.h"
 //header file for pid server
 #include "lino_msgs/PID.h"
-//header file for imu
-#include "lino_msgs/Imu.h"
 
 #include "lino_base_config.h"
 #include "Motor.h"
 #include "Kinematics.h"
 #include "PID.h"
-#include "Imu.h"
 
-#define ENCODER_OPTIMIZE_INTERRUPTS // comment this out on Non-Teensy boards
-#include "Encoder.h"
 
-#define IMU_PUBLISH_RATE 20 //hz
 #define COMMAND_RATE 20 //hz
 #define DEBUG_RATE 5
-
-Encoder motor1_encoder(MOTOR1_ENCODER_A, MOTOR1_ENCODER_B, COUNTS_PER_REV);
-Encoder motor2_encoder(MOTOR2_ENCODER_A, MOTOR2_ENCODER_B, COUNTS_PER_REV); 
-Encoder motor3_encoder(MOTOR3_ENCODER_A, MOTOR3_ENCODER_B, COUNTS_PER_REV); 
-Encoder motor4_encoder(MOTOR4_ENCODER_A, MOTOR4_ENCODER_B, COUNTS_PER_REV); 
 
 Servo steering_servo;
 
@@ -64,9 +53,6 @@ ros::NodeHandle nh;
 ros::Subscriber<geometry_msgs::Twist> cmd_sub("cmd_vel", commandCallback);
 ros::Subscriber<lino_msgs::PID> pid_sub("pid", PIDCallback);
 
-lino_msgs::Imu raw_imu_msg;
-ros::Publisher raw_imu_pub("raw_imu", &raw_imu_msg);
-
 lino_msgs::Velocities raw_vel_msg;
 ros::Publisher raw_vel_pub("raw_vel", &raw_vel_msg);
 
@@ -80,7 +66,6 @@ void setup()
     nh.subscribe(pid_sub);
     nh.subscribe(cmd_sub);
     nh.advertise(raw_vel_pub);
-    nh.advertise(raw_imu_pub);
 
     while (!nh.connected())
     {
@@ -93,7 +78,6 @@ void setup()
 void loop()
 {
     static unsigned long prev_control_time = 0;
-    static unsigned long prev_imu_time = 0;
     static unsigned long prev_debug_time = 0;
     static bool imu_is_initialized;
 
@@ -110,25 +94,6 @@ void loop()
         stopBase();
     }
 
-    //this block publishes the IMU data based on defined rate
-    if ((millis() - prev_imu_time) >= (1000 / IMU_PUBLISH_RATE))
-    {
-        //sanity check if the IMU is connected
-        if (!imu_is_initialized)
-        {
-            imu_is_initialized = initIMU();
-
-            if(imu_is_initialized)
-                nh.loginfo("IMU Initialized");
-            else
-                nh.logfatal("IMU failed to initialize. Check your IMU connection.");
-        }
-        else
-        {
-            publishIMU();
-        }
-        prev_imu_time = millis();
-    }
 
     //this block displays the encoder readings. change DEBUG to 0 if you don't want to display
     if(DEBUG)
@@ -170,10 +135,10 @@ void moveBase()
     Kinematics::rpm req_rpm = kinematics.getRPM(g_req_linear_vel_x, g_req_linear_vel_y, g_req_angular_vel_z);
 
     //get the current speed of each motor
-    int current_rpm1 = motor1_encoder.getRPM();
-    int current_rpm2 = motor2_encoder.getRPM();
-    int current_rpm3 = motor3_encoder.getRPM();
-    int current_rpm4 = motor4_encoder.getRPM();
+    int current_rpm1 = 2;
+    int current_rpm2 = 2;
+    int current_rpm3 = 2;
+    int current_rpm4 = 2;
 
     //the required rpm is capped at -/+ MAX_RPM to prevent the PID from having too much error
     //the PWM value sent to the motor driver is the calculated PID based on required RPM vs measured RPM
@@ -212,20 +177,6 @@ void stopBase()
     g_req_angular_vel_z = 0;
 }
 
-void publishIMU()
-{
-    //pass accelerometer data to imu object
-    raw_imu_msg.linear_acceleration = readAccelerometer();
-
-    //pass gyroscope data to imu object
-    raw_imu_msg.angular_velocity = readGyroscope();
-
-    //pass accelerometer data to imu object
-    raw_imu_msg.magnetic_field = readMagnetometer();
-
-    //publish raw_imu_msg
-    raw_imu_pub.publish(&raw_imu_msg);
-}
 
 float steer(float steering_angle)
 {
@@ -245,16 +196,4 @@ float mapFloat(float x, float in_min, float in_max, float out_min, float out_max
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void printDebug()
-{
-    char buffer[50];
 
-    sprintf (buffer, "Encoder FrontLeft  : %ld", motor1_encoder.read());
-    nh.loginfo(buffer);
-    sprintf (buffer, "Encoder FrontRight : %ld", motor2_encoder.read());
-    nh.loginfo(buffer);
-    sprintf (buffer, "Encoder RearLeft   : %ld", motor3_encoder.read());
-    nh.loginfo(buffer);
-    sprintf (buffer, "Encoder RearRight  : %ld", motor4_encoder.read());
-    nh.loginfo(buffer);
-}
